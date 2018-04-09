4b8d90c8ab499040d63dbe1bdc18f275d8163d46
LucianoKlein <kleinersilver@gmail.com>
2018-04-09 17:18:04 +0800

    组件的组合方式

--- src/app.jsx
+++ src/app.jsx
@@ -5,34 +5,40 @@ class Component extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            name : 'hexin',
             age: 18
         }
-    }
-    handleClick() {
-        this.setState({
-            age: this.state.age + 1
-        })
-    }
-    onValueChange(e) {
-        this.setState({
-            age: e.target.value 
-        })
     }
     render() {
         return (
             <div>
-                <h1>I am {this.state.name}</h1>
                 <p>I am {this.state.age} years old</p>
-                <button onClick={(e) => {this.handleClick(e)}}>加一岁</button>
-                <input type="text" onChange={(e) => {this.onValueChange(e)}}/>
+            </div>
+        );
+    }
+}
+class Title extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render(props) {
+        return <h1>{this.props.children}</h1>
+    }
+}
+class App extends React.Component {
+    render() {
+        return (
+            <div className="">
+                 <Title>
+                     <span>App Span</span>
+                     <a href="">link</a>
+                 </Title>
+                 <hr/>
+                 <Component/>
             </div>
         );
     }
 }
 ReactDOM.render(
-    <div>
-        <Component/>
-    </div>,
+    <App/>,
     document.getElementById("app")
 )
