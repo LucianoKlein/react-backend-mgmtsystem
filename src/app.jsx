e429c7ec5ada773275159872ae31ed76afab254f
LucianoKlein <kleinersilver@gmail.com>
2018-04-09 17:50:10 +0800

    父子组件传值

--- src/app.jsx
+++ src/app.jsx
@@ -1,39 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Component extends React.Component {
+class Child extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            age: 18
-        }
     }
+    handleClick(e) {
+        this.props.changeColor('red');
+    }
+
     render() {
         return (
             <div>
-                <p>I am {this.state.age} years old</p>
+                <h1>父组件的背景色： {this.props.bgColor}</h1>
+                <button onClick={(e) => {this.handleClick(e)}}>改变父组件颜色</button>
             </div>
         );
     }
 }
-class Title extends React.Component {
+
+class Father extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            bgColor: "#999"
+        }
+    }
+    onBgColorChange(color) {
+        this.setState({
+            bgColor: color
+        })
     }
     render(props) {
-        return <h1>{this.props.children}</h1>
+        return (
+            <div style={{background: this.state.bgColor}}>
+                <Child bgColor={this.state.bgColor} changeColor={(color)=>{this.onBgColorChange(color)}}/>
+            </div>
+        )
     }
 }
+
 class App extends React.Component {
     render() {
         return (
-            <div className="">
-                 <Title>
-                     <span>App Span</span>
-                     <a href="">link</a>
-                 </Title>
-                 <hr/>
-                 <Component/>
+            <div>
+                <Father/>
             </div>
         );
     }
