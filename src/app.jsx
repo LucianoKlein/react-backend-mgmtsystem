6cdc98426f3ecf939b481e66afe8c41bf2433c71
LucianoKlein <kleinersilver@gmail.com>
2018-04-09 10:53:12 +0800

    logic

--- src/app.jsx
+++ src/app.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import './index.scss'
-
-let style = {
-    color: 'red',
-    fontSize: '30px'
-}
-let jsx = <div style={style}>jsx.....</div>;
-
-
-let jsx2 = <div className="jsx" >jsx2......</div>;
+let name = 'hexin';
+let flag = false;
+let jsx = (
+    <div>
+        {
+            flag ? <p>I am {name}</p> : <p>I am not {name}</p>
+        }
+    </div>
+);
 
 ReactDOM.render(
-    jsx2, 
+    jsx,
     document.getElementById("app")
-);
+)
